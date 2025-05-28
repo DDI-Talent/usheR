e3eabb79fb0238235d059842ca1d69a593e471f2
Nicola Burns <s1725659@ed.ac.uk>
2025-05-28 09:11:24 +0100

    NICOLA - df CONVERSION function addition

--- R/pairs-maker.R
+++ R/pairs-maker.R
@@ -148,3 +148,37 @@ print.pairings <- function(x, n = 1, ...) {
   }, x[n], rev(seq_along(x))[n])
   invisible(x)
 }
+
+
+#' Convert pairings list to data frame
+#'
+#' Converts the output of create_pairs() into a data frame with the columns Week (integer), Group (integer), and Pair (Character)
+#'
+#' @param pairings A list object returned from create_pairs()
+#'
+#' @returns A data frame with the columns Week, Group, and Pair
+#' @export
+#'
+#' @examples
+#' df <- pairings_to_df(pairings)
+#'
+pairings_to_df <- function(pairings) {
+  rows <- list() #list for accumulating rows of final df
+
+  #Loop through list to extract each weeks data (1 to however many weeks etc)
+  for (week in seq_along(pairings)) { # seq_along() - index for each week
+    week_data <- pairings[[week]] # get groups for that week
+
+    #Loop through each pair in each week and split students?
+    for (group in seq_along(week_data)) { # loop each group in that week
+      rows <- append(rows, list(data.frame(
+        Week = week,
+        Group = group,
+        Pair = week_data[group],
+        stringsAsFactors = F
+      )))
+    }
+  }
+
+  do.call(rbind.data.frame, rows) #stack rows list and converts to df
+}
